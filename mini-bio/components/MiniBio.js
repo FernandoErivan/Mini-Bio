0ed2ac8f6b512d6d175a7650cbfd696b8fd1c8ac
Fernando Erivan <fernandoo.estudos@gmail.com>
2026-08-27 03:34:18 +0000

    Esqueleto da atv

---
+++ mini-bio/components/MiniBio.js
@@ -0,0 +1,17 @@
+export default function MiniBio() {
+  return (
+    <section className="mini-bio">
+      <img
+        src="/perfil.jpg"
+        alt="Foto do Javinha"
+        className="profile-image"
+      />
+
+      <h1>Mascote Javinha</h1>
+
+      <p>
+        Desenvolvedor apaixonado por tecnologia e criação de interfaces.
+      </p>
+    </section>
+  );
+}
---
+++ mini-bio/components/Profile.js
@@ -0,0 +1,9 @@
+import MiniBio from "./MiniBio";
+
+export default function Profile() {
+  return (
+    <main className="profile">
+      <MiniBio />
+    </main>
+  );
+}
